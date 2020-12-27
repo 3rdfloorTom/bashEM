2436233802fcfcbda3e08d86d9d115738d6c8736
3rdFloorTom <thomas.laughliniii@gmail.com>
2020-12-26 17:01:54 -0800

    new script for thresholding by Z value

---
+++ rln/rln_thresholdAutoPick_byZ.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+#
+#
+##########################################################################################################
+#
+# Author(s): Tom Laughlin University of California-San Diego 2020
+#
+# 
+#
+###########################################################################################################
+
+#usage description
+usage () 
+{
+	echo ""
+	echo "This script thresholds a RELION style autopick star file by specified Z extents."
+	echo "It will out put a thresholded star file named input_threshZ.star"
+	echo ""
+	echo "Usage is:"
+	echo ""
+	echo "$(basename $0) -i input.star -l lowestZ -h highestZ"
+	echo ""
+	echo "options list:"
+	echo "	-i: .star generated from a RELION/Warp template matching run			(required)"
+	echo "	-l: lowest value (inclusive) of Z coordinate allowed to be included in output starfile	(required)"
+	echo "	-h: high value (exclusive) of Z coordinate allowed to be included in output starfile	(required)"
+	echo ""
+	exit 0
+}
+
+# check for any arguements at all
+if [[ $# == 0 ]] ; then
+	usage
+fi
+
+#grab command-line arguements
+while getopts ":i:l:h:" options; do
+    case "${options}" in
+        i)
+            if [[ -f ${OPTARG} ]] ; then
+           		inStar=${OPTARG}
+			echo ""
+			echo "Found input starfile: ${inStar}"
+			echo ""
+            else
+           		echo ""
+           		echo "Error: could not input starfile: ${inStar}"
+           		echo ""
+           		usage
+            fi
+            ;;
+
+	 l)
+	    if [[ ${OPTARG} =~ ^[0-9]+([.][0-9]+)?$ ]] ; then
+			threshLow=${OPTARG}
+	    else
+			echo ""
+			echo "Error: Threshold must be a postive value."
+			echo "Exiting..."
+			echo ""
+			usage
+	    fi
+	    ;;
+
+	 h)
+	    if [[ ${OPTARG} =~ ^[0-9]+([.][0-9]+)?$ ]] ; then
+			threshHigh=${OPTARG}
+	    else
+			echo ""
+			echo "Error: Threshold must be a postive value."
+			echo "Exiting..."
+			echo ""
+			usage
+	    fi
+	    ;;
+
+          *)
+	     usage
+            ;;
+    esac
+done
+shift "$((OPTIND-1))"
+
+# Check inputs
+if [[ -z $inStar ]] ; then
+
+	echo ""
+	echo "Error: No star file provided!"
+	echo "Exiting..."
+	echo ""
+	usage	
+	
+fi
+
+if [[ -z $threshLow ]] || [[ -z $threshHigh ]] ; then
+	echo ""
+	echo "Error: Threshold not set!"
+	echo "Exiting..."
+	echo ""
+	usage
+fi
+
+if [[ -z $(grep "_rlnCoordinateZ" ${inStar}) ]] ; then
+
+	echo ""
+	echo "Error: Input file does not contain a _rlnCoordinateZ column...not much to do here."
+	echo "Exiting..."
+	echo ""
+	usage
+fi
+
+# Give output a name
+outFile="${inStar%.*}_threshZ.star"
+
+# Get FOM field
+zField=$(grep "_rlnCoordinateZ" ${inStar} | awk '{print $2}' | sed 's|#||')
+
+# Grab header (lines without .mrc in them)
+awk '{if ($0 !~ /.mrc/) {print $0}}' ${inStar} > ${outFile}
+
+# Threshold FOM field and output to new file (sort so it is ease to tell the thresholding later by top and bottom values)
+awk -v zField=$zField -v threshL=$threshLow -v threshH=$threshHigh '{if ($0 ~ /.mrc/ && ($zField >= threshL && $zField < threshH)) {print $0}}' ${inStar} | sort -nr -k$zField >> ${outFile}
+
+# Determine the pre/post thresholding particle counts
+preCount=$(grep ".mrc" $inStar | wc -l | awk '{print $1}')
+postCount=$(grep ".mrc" $outFile | wc -l | awk '{print $1}')
+
+echo "Selected particles with Z-Coordinates between: $threshLow $threshHigh"
+echo ""
+echo "Pre-threshold count of particles:	${preCount}"
+echo "Post-threshold count of particles: 	${postCount}"
+echo ""
+echo "A star file sorted by the Z-coordinate has been written out as:" 
+echo "${outFile}"
+echo ""
+echo "Script done!"
+echo ""
+
+exit 1
